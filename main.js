79b9bc17d9c65a3e6fbda34df39e0e22fb15f00d
yori909090 <karylle.bachicha5@gmail.com>
2025-09-27 14:37:15 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// Smooth scrolling (for extra smooth effect)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
